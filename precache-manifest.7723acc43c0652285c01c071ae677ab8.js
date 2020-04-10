cf0901889f4dc475ee38b2edc035d574c7efda62
Jahmilli <sebastian.southern@gmail.com>
2020-04-10 17:53:38 +1000

    Updates

---
+++ precache-manifest.7723acc43c0652285c01c071ae677ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a74b2e54d780aade5cffdd1d632632",
+    "url": "/teams-ui/index.html"
+  },
+  {
+    "revision": "9e5474cb2f26afa47faa",
+    "url": "/teams-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "746458743f5e9d803f9a",
+    "url": "/teams-ui/static/js/2.2ff1b1b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teams-ui/static/js/2.2ff1b1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5474cb2f26afa47faa",
+    "url": "/teams-ui/static/js/main.92fc929f.chunk.js"
+  },
+  {
+    "revision": "1540548e216f1dad8355",
+    "url": "/teams-ui/static/js/runtime-main.11300510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teams-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
